5debdf85dc8158a43a072fb99de3f01a945da43b
OscarXVI <calavera-123>
2019-05-17 08:32:09 -0500

    aplicacion terminada

--- app/src/main/java/com/example/navegacioninferior/MainActivity.java
+++ app/src/main/java/com/example/navegacioninferior/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 
@@ -38,8 +38,6 @@ public class MainActivity extends AppCompatActivity {
         CoordinatorLayout.LayoutParams layoutParams = (CoordinatorLayout.LayoutParams) navigation.getLayoutParams();
         layoutParams.setBehavior(new BottomNavigationBehavior());
 
-        toolbar.setTitle("Shop");
-        toolbar.setTitleTextColor(Color.WHITE);
         loadFragment(new StoreFragment());
     }
 
